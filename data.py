5887b0bd2114b06de057b3a86dcfc3957333df36
connerng <cjngadisastra@gmail.com>
2024-07-22 22:57:48 -0700

    NFC west animation complete

---
+++ data.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+url_sea = 'https://www.pro-football-reference.com/teams/sea/2023/gamelog/'
+url_sf = 'https://www.pro-football-reference.com/teams/sfo/2023/gamelog/'
+url_la = 'https://www.pro-football-reference.com/teams/ram/2023/gamelog/'
+url_az = 'https://www.pro-football-reference.com/teams/crd/2023/gamelog/'
+
+urls = [url_az, url_la, url_sea, url_sf]
+sequences = []
+
+for url in urls:
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, features="html.parser")
+    table = soup.find_all('table')[0]
+    world_titles = table.find_all('th')
+    table_titles = [title.text.strip() for title in world_titles][8:17]
+    table_titles[8] = 'PTS'
+    weeks = [title.text.strip() for title in world_titles][44:]
+
+    # create dataframe
+    df = pd.DataFrame(columns=table_titles)
+    zeroRow = ['0', '', '', '', '', '', '', '', '0']
+    df.loc[len(df)] = zeroRow
+
+    # add web data to dataframe
+    column_data = table.find_all('tr')
+    i = 0
+    for row in column_data:
+        row_data = row.find_all('td')
+        individual_row_data = [data.text.strip() for data in row_data][:8]
+        if len(individual_row_data) > 0:
+            individual_row_data.insert(0, weeks[i])
+            i += 1
+            df.loc[len(df)] = individual_row_data
+
+    # edit df
+    df = df.drop(['Day', 'Date', '', 'OT', 'Opp'], axis=1)
+    df['Week'] = pd.to_numeric(df['Week'])
+    df['PTS'] = pd.to_numeric(df['PTS'])
+
+
+    # find bye week
+    for i in range(len(df)):
+        if df.loc[i]['Week'] != i:
+            bye_week = i
+            break
+
+    # add bye week row
+    newRow = [bye_week, 0]
+    for i in range(len(df), bye_week, -1):
+        df.loc[i] = df.loc[i-1]
+        
+    df.loc[bye_week] = newRow
+
+
+    # create points sequence
+    pts_sum = 0
+    pts_seq = []
+
+    for ind in df.index:
+        pts_sum += df['PTS'][ind]
+        pts_seq.append(pts_sum)
+    
+    sequences.append(pts_seq)
--- visuals.py
+++ visuals.py
@@ -2,26 +2,31 @@ from webscraper import *
 from matplotlib import animation
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import seaborn as sns
+import numpy as np
 
-fig = plt.figure(figsize=(7,5))
+fig = plt.figure(figsize=(8,6))
+plt.style.use("seaborn-v0_8-dark")
 axes = fig.add_subplot(1,1,1)
 axes.set_xlim(0,500)
+palette = ['black', 'yellow', 'blue', 'red']
 
 
-
-y1, y2, y3 = [], [], []
+y1, y2, y3, y4 = [], [], [], []
 def animate(i):
-    y1 = pts_seq[i]
+    y1 = sequences[0][i]
+    y2 = sequences[1][i]
+    y3 = sequences[2][i]
+    y4 = sequences[3][i]
+
     axes.cla()
-    plt.barh([0], y1, color=['blue'])
-    # td = {"Points":y1}
-    # tcks = [i[0] for i in td]
+    plt.grid(axis='x')
+    plt.xlabel("Points")
+    plt.xticks(np.arange(0, 500, 50))
+    plt.barh(['Cardinals', 'Rams', 'Seahawks', '49ers'], [y1, y2, y3, y4], color=palette)
+    plt.bar_label(plt.gca().containers[0], label_type='edge', padding=2)
 
-    plt.title("Seattle Seahawks 2023-2024 Total Points by Week: {} ".format(df['Week'][i]))
+    plt.title("NFC West Total Points Scored 2023-2024 | Week {} ".format(df['Week'][i]))
     axes.set_xlim(0,500)
-    plt.gca().tick_params(left=False)
-    print(i, pts_seq[i])
 
-visual = FuncAnimation(fig, animate, frames=len(pts_seq), interval=500)
+visual = FuncAnimation(fig, animate, frames=len(sequences[0]), interval=1000)
 plt.show()
